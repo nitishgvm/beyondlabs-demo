7196dafe881232608966a9628a46ea8593036112
SantoshAcademian <smahajan@academian.com>
2026-01-02 10:26:51 +0530

    Initial setup: Client and Server structure

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
